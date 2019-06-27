9f09af883537d71f35ec37b0c12f471251046095
petek157 <petek157@hotmail.com>
2019-06-27 17:33:54 -0500

    formatted portfolio added someoics

--- barebone/js/scripts.js
+++ barebone/js/scripts.js
@@ -1,9 +1,43 @@
 $(function() {
+  
     $.getJSON( "/barebone/data.json", function( data ) {
-        var items = [];
-        $.each( data, function( key, val ) {
-          items.push( "<li id='" + key + "'>" + val + "</li>" );
-        });
-
+      var html = "";
+      var tags = ["All"];
+      $.each( data["projects"], function(i, project) {
+        html += '<div class="project">';
+        
+          html += '<div class="title">' + project.title + '</div>';
+          html += '<div class="tags">'
+              html += '<ul>';
+              project.tech.sort();
+                $.each(project.tech, function(i, t) {
+                  console.log(t);
+                  html += '<li>' + t + '</li>';
+                  if ($.inArray(t, tags) == -1) {
+                    tags.push(t);
+                  }
+                });
+              html += '</ul>';
+          html += '</div>';
+          html += '<div class="img">';
+              html += '<img src="' + project.image + '"/>';
+          html += '</div>';
+          html += '<div class="desc">';
+            $.each(project.description, function(i, d) {
+              html += '<p>' + d + '</p>';
+            });
+          html += '</div>';
+          if (project.link != "" && project.link != undefined) {
+            html += '<div class="link"><a href="' + project.link + '">View</a></div>';
+          }
+          html += '</div>';
+      });
+      tags.sort();
+      $.each(tags, function(i, ta) {
+        $(".filter").append('<li>' + ta + '</li>');
+      });
+      
+      $(".projects").html(html);
     });
+  
 })
